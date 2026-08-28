eaadc577a9c54f092ed03a7f31c8904491fd3bd0
jwya <1817219421@qq.com>
2026-08-28 16:36:52 +0800

    Add optional backend integration package

---
+++ lingjing_harness/integrations/__init__.py
@@ -0,0 +1,5 @@
+"""Optional integrations with mature search, recommendation, and optimization libraries."""
+
+from .flag_embedding import FlagEmbeddingSearchAdapter
+
+__all__ = ["FlagEmbeddingSearchAdapter"]
