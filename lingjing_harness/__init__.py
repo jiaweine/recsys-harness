20d66552efe46d268580bc0de81f1978b6f7b6e0
jwya <1817219421@qq.com>
2026-09-09 16:42:23 +0800

    Install fresh-run assistant lookup gate

--- lingjing_harness/__init__.py
+++ lingjing_harness/__init__.py
@@ -2,6 +2,9 @@
 
 from . import online_experiment_store as _online_experiment_store_module
 from . import store as _store_module
+from .store_assistant_lookup import (
+    install_fresh_run_assistant_lookup_gate as _install_fresh_run_assistant_lookup_gate,
+)
 from .store_online_experiment_listing import (
     install_online_experiment_list_aggregation as _install_online_experiment_list_aggregation,
 )
@@ -15,6 +18,7 @@ from .store_workspace_publication_atomic_fence import (
 )
 
 _install_workspace_run_schema_migration_guard(_store_module)
+_install_fresh_run_assistant_lookup_gate(_store_module)
 _install_run_recovery_claim_fence(_store_module)
 _install_workspace_publication_fence(_store_module)
 _install_workspace_publication_atomic_fence(_store_module)
